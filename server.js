8cb24d8f583b6a1f386dacef55dae8c294e2e48f
msaiff <msaif1681@gmail.com>
2017-05-13 05:45:53 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use( (req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) =>{
+    if (err) {
+      console.log('Unable to append to server.log.');
+    }
+  });
+  next();
+});
+
+// app.use( (req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () =>{
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+
+app.get('/', (req, res)=>{
+  //res.send('<h1>Hello ExpressJs!</h1>');
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to our Website!'
+  });
+});
+
+// app.get('/maintenance', (req, res)=>{
+//   //res.send('<h1>Hello ExpressJs!</h1>');
+//    res.render('maintenance.hbs', {
+//     pageTitle: 'maintenance',
+//     maintanenceMessage: 'Will be back soon, thanks for your understanding.'
+//   });
+// });
+
+app.get('/about', (req, res)=>{
+  //res.send('<h1>About Page</h1>');
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad2', (req, res)=>{
+  res.send('<h1>404 Bad request.</h1>');
+});
+
+var eM = 'Error Handling Request';
+app.get('/bad', (req, res)=>{
+  res.send({
+    errorMessge: eM
+  });
+});
+
+app.listen(3000, () =>{
+  console.log('Server is up on port 3000');
+});
+
+
+
+
+
+// app.get('/', (req, res)=>{
+//   //res.send('<h1>Hello ExpressJs!</h1>');
+//   res.send({
+//     name: 'MSaif',
+//     Likes: [
+//       'Quran',
+//       'Biking',
+//       'Coding'
+//     ]
+//   });
+// });
